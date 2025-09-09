ab2f9a19e4d9cc53ad92459356c599d4d7f74cd9
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-09-09 23:49:16 +0530

    feat: added frontend client with next

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,34 @@
-import Image from "next/image";
+'use client'
+
+import axios from "axios";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="font-sans flex grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      
-    </div>
-  );
+
+
+	const [data, setData] = useState <string | null>(null)
+
+	const call_gRPC = async () => {
+		try {
+			const response = await axios.get('http://localhost:4000/hello/Abdullah')
+			setData(response.data.message)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+	return (
+		<div className="font-sans flex bg-neutral-100 text-neutral-900 justify-around min-h-screen">
+			<div className="max-w-4xl">
+				<button type="button" className="bg-blue-400 p-2 rounded-md cursor-pointer" onClick={call_gRPC}>
+					Send Request
+				</button>
+
+				{data && (
+					<div className="">
+						{data}
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
