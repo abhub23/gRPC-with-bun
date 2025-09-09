816fe9593b78c7a2759f4f34e1e57f8fabca0991
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-09-09 23:48:55 +0530

    feat: added hono server for api route

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,35 @@
-console.log("Hello via Bun!");
+// server.ts
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import client from "./grpcClient";
+
+const port = 4000;
+const app = new Hono();
+
+app.use("*", cors());
+
+app.get("/", (c) => {
+  return c.json({ message: "Server is alive" });
+});
+
+// gRPC route
+app.get("/hello/:name", async (c) => {
+  const name = c.req.param("name");
+
+  return new Promise((resolve) => {
+    client.SayHello({ name }, (err: any, response: any) => {
+      if (err) {
+        resolve(c.json({ error: err.message }, 500));
+      } else {
+        resolve(c.json({ message: response.message }, 200));
+      }
+    });
+  });
+});
+
+Bun.serve({
+  fetch: app.fetch,
+  port,
+});
+
+console.log(`🚀 Hono server running at http://localhost:${port}`);
